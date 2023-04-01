d2faecebd421e39d17fde1761b62d81d7997b42e
SennKuu02 <SennKuu02>
2023-04-01 23:50:06 +0700

    message

---
+++ Model/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF_Example.Model
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
